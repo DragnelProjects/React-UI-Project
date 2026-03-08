3467b97c9935068c5877426af58730b369b9acc4
DragnelProjects <291502intekhab@gmail.com>
2026-03-08 14:28:40 +0530

    final commit

--- src/components/middle/LastText.jsx
+++ src/components/middle/LastText.jsx
@@ -4,10 +4,10 @@ import { Play } from 'lucide-react';
 
 function LastText() {
   return (
-    <div className=' flex items-center mt-4 gap-2'>
-        <button className='bg-green-500 text-[10px] text-neutral-100 font-semibold py-1.5 px-3 rounded-full flex items-center gap-0.5 '>Explore Courses <ArrowRight size={11} /></button>
-        <span className='bg-orange-400 p-2 rounded-full'><Play color='white' fill='white' size={12}/></span>
-        <span className='text-xs font-semibold underline decoration-1 decoration-mist-700'>Watch Demo</span>
+    <div className=' flex items-center mt-4 gap-4'>
+        <button className='bg-green-500 text-[11px] text-neutral-100 font-semibold py-2 px-3 rounded-full flex items-center gap-0.5 '>Explore Courses <ArrowRight size={12} /></button>
+        <span className='bg-orange-400 p-2 rounded-full'><Play color='white' fill='white' size={14}/></span>
+        <span className='text-[12px] text-mauve-800 font-bold underline decoration-1 decoration-mist-700'>Watch Demo</span>
     </div>
   )
 }
